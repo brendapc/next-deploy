0ffd9f9ed083e0b3d9dc2f9e15c74f7b4f5cba7f
Brenda Pereira <brendacamara@outlook.com>
2024-03-12 15:03:26 -0300

    feat: get data via serverSideProps hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { resources } from "~/api/data";
 import Footer from "~/components/Footer";
 import Navbar from "~/components/Navbar";
 import Newsletter from "~/components/Newsletter";
 import ResourceHighlight from "~/components/ResourceHighlight";
 import ResourceList from "~/components/ResourceList";
+import Resource from "~/types/Resource";
 
 
-function Home() {
+function Home({ resources }: {resources: Resource[]}) {
     return (
       <>
         <Navbar/>
@@ -20,5 +20,17 @@ function Home() {
     )
   }
   
-  
-  export default Home;
+// is called every time you will visit the page
+// function is executed on the server
+// data are always fresh
+export async function getServerSideProps() {
+  const resData = await fetch("http://localhost:3000/api/resources");
+  const data = await resData.json();
+  return {
+    props: {
+      resources: data
+    }
+  }
+}
+
+export default Home;
