cbb821912fac680bd604c3ed13182573d34867c0
Brenda Pereira <brendacamara@outlook.com>
2024-03-13 15:03:31 -0300

    feat: add backend url from deploy

--- src/pages/api/resources.ts
+++ src/pages/api/resources.ts
@@ -1,7 +1,7 @@
 import { NextApiResponse, NextApiRequest } from 'next';
  
 const resources = async (req: NextApiRequest, res: NextApiResponse) =>{
-  const dataRes = await fetch("http://localhost:3001/api/resources");
+  const dataRes = await fetch("https://content-manager-app-api-zeta.vercel.app/api/resources");
   const data = await dataRes.json();
 
   res.send(data);
